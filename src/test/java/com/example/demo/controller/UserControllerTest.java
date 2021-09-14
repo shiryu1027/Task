1c21423eed418307b8d601e8bd78917ccd12f016
shiryu1027 <asmttmcz.1027@outlook.jp>
2021-09-15 00:27:04 +0900

    SELECT処理(All)のテストコードの修正
    verifyの追加、mockitoの使用

--- src/test/java/com/example/demo/controller/UserControllerTest.java
+++ src/test/java/com/example/demo/controller/UserControllerTest.java
@@ -1,20 +1,30 @@
 package com.example.demo.controller;
 
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoSession;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.example.demo.dto.UserSearchRequest;
+import com.example.demo.entity.User;
 import com.example.demo.service.UserService;
 
 @SpringBootTest
@@ -22,12 +32,32 @@ import com.example.demo.service.UserService;
 @Transactional
 @DisplayName("UserControlerクラスをテスト")
 class UserControllerTest {
-
-	@Autowired
-	MockMvc mockMvc;
 	
-	@Autowired
+	private MockitoSession session;
+	private MockMvc mockMvc;
+	
+	@Mock
 	private UserService userService;
+	
+	@InjectMocks
+	private UserController target;
+	
+	/* 参考:https://qiita.com/Tsuyoshi_Murakami/items/2fa5c0a41432f705400a#%E3%83%A2%E3%83%83%E3%82%AF%E3%82%AF%E3%83%A9
+	        %E3%82%B9%E3%81%AB%E5%AF%BE%E8%B1%A1%E3%82%AF%E3%83%A9%E3%82%B9%E3%81%AE%E3%82%A2%E3%83%8E%E3%83%86%E3%83%BC
+	        %E3%82%B7%E3%83%A7%E3%83%B3%E3%81%8C%E4%BB%98%E4%B8%8E%E3%81%95%E3%82%8C%E3%82%8B%E3%82%88%E3%81%86%E3%81%AB
+	        %E3%81%AA%E3%81%A3%E3%81%9F
+	*/
+	@BeforeEach
+	void setUp() {
+		// MockitoAnnotations.initMocks(this)の新しいバージョン(モックオブジェクトの初期化)
+	    session = Mockito.mockitoSession().initMocks(this).startMocking();
+	    this.mockMvc = MockMvcBuilders.standaloneSetup(target).build();
+	}
+	
+	@AfterEach
+	void tearDown() {
+	    session.finishMocking();
+	}
 	
 	@Nested
 	class 全ユーザー情報を表示したトップページを返す{
@@ -40,15 +70,33 @@ class UserControllerTest {
 		}
 		
 		@Test
-		@DisplayName("modelに変数listでList/User型のデータを流す")
-		void modelに変数listでList_User型のデータを流す() throws Exception {
+		@DisplayName("UserServiceクラスのsearchAllメソッド一回呼び出す")
+		void UserServiceクラスのsearchAllメソッド一回呼び出す() throws Exception {
+			// Arrange
+			List<User> list = new ArrayList<User>();
+			doReturn(list).when(userService).searchAll();
+			
+			// Act & Assert
+			mockMvc.perform(get("/"))
+				.andExpect(status().isOk());
+			verify(userService, times(1)).searchAll();
+		}
+		
+		@Test
+		@DisplayName("modelに変数usersinfoでList/User型のデータを流す")
+		void modelに変数usersinfoでList_User型のデータを流す() throws Exception {
+			// Arrange
+			List<User> list = new ArrayList<User>();
+			doReturn(list).when(userService).searchAll();
+			
+			// Act & Assert
 			mockMvc.perform(get("/"))
 				.andExpect(status().isOk())
-				.andExpect(model().attribute("usersinfo", userService.searchAll()))
-				.andExpect(model().hasNoErrors());
+				.andExpect(model().attribute("usersinfo", list));
 		}
 	}
 	
+	// ここから
 	@Nested
 	class ユーザー情報id検索画面を返す {
 		@Test
