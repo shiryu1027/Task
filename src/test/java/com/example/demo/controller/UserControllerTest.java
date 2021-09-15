f3c67226d2cf09979c0db63a9ec19950a343eaf9
shiryu1027 <asmttmcz.1027@outlook.jp>
2021-09-15 15:40:34 +0900

    UPDATE処理、DELETE処理のテスト追加
    ControllerのTest作成終了

--- src/test/java/com/example/demo/controller/UserControllerTest.java
+++ src/test/java/com/example/demo/controller/UserControllerTest.java
@@ -59,6 +59,7 @@ class UserControllerTest {
 	    session.finishMocking();
 	}
 	
+	// READ処理
 	@Nested
 	class 全ユーザー情報を表示したトップページを返す{
 		@Test
@@ -92,11 +93,11 @@ class UserControllerTest {
 			// Act & Assert
 			mockMvc.perform(get("/"))
 				.andExpect(status().isOk())
-				.andExpect(model().attribute("usersinfo", list));
+				.andExpect(model().attribute("usersinfo", list))
+				.andExpect(model().hasNoErrors());
 		}
 	}
 	
-	// ここから
 	@Nested
 	class ユーザー情報id検索画面を返す {
 		@Test
@@ -121,23 +122,39 @@ class UserControllerTest {
 		}
 		
 		@Test
+		@DisplayName("UserServiceクラスのsearchメソッド一回呼び出す")
+		void UserServiceクラスのsearchメソッド一回呼び出す() throws Exception {
+			// Arrange
+			UserSearchRequest userSearchRequest = new UserSearchRequest();
+			User user = new User();
+			doReturn(user).when(userService).search(userSearchRequest);
+			
+			// Act & Assert
+			mockMvc.perform(
+					post("/user/id_search"))
+				.andExpect(status().isOk());
+			verify(userService, times(1)).search(userSearchRequest);
+		}
+		
+		@Test
 		@DisplayName("modelに変数userinfoでUser型のデータを流す")
 		void modelに変数userinfoでUser型のデータを流す() throws Exception {
 			
 			// 前処理
 			UserSearchRequest userSearchRequest = new UserSearchRequest();
-			userSearchRequest.setId(1);
+			User user = new User();
+			doReturn(user).when(userService).search(userSearchRequest);
 			
 			// 実行&検証
 			mockMvc.perform(
 				post("/user/id_search")
-				.flashAttr("userSearchRequest", userSearchRequest)
 			).andExpect(status().isOk())
 			.andExpect(model().attribute("userinfo", userService.search(userSearchRequest)))
 			.andExpect(model().hasNoErrors());
 		}
 	}
 	
+	// CREATE処理
 	@Nested
 	class ユーザー登録画面を表示 {
 		@Test
@@ -152,24 +169,41 @@ class UserControllerTest {
 	@Nested
 	class ユーザー登録を行い_ホームにリダイレクトする {
 		
-		@Nested
-		class 入力エラー時_user_create_htmlを返す {
-			@Test
-			@DisplayName("UserSearchRequestが空のとき、user/create.htmlを返す")
-			void user_create_htmlを返す() throws Exception {
-				
-				mockMvc.perform(
-					post("/user/create")
-					.param("id", "")
-					.param("name", "")
-					.param("age", "")
-					.param("birthday", "")
-				).andExpect(status().isOk())
-				.andExpect(view().name("user/create"))
-				.andExpect(model().attributeHasErrors("userSearchRequest"));
-			}
+		@Test
+		@DisplayName("入力エラー時_user_create_htmlを返す")
+		void 入力エラー時_user_create_htmlを返す() throws Exception {
+			
+			// Arrange
+			UserSearchRequest userSearchRequest = new UserSearchRequest();
+			userSearchRequest.setId(-1);
+			
+			// Act & Assert
+			mockMvc.perform(
+				post("/user/create")
+				.flashAttr("userSearchRequest", userSearchRequest)
+			).andExpect(status().isOk())
+			.andExpect(view().name("user/create"));
 		}
 		
+		@Test
+		@DisplayName(" 正常時、ユーザー登録のサービスメソッドが実行できているか")
+		void  正常時にユーザー登録のサービスメソッドが実行できているか() throws Exception {
+			// 前処理
+			UserSearchRequest userSearchRequest = new UserSearchRequest();
+			userSearchRequest.setId(25);
+			userSearchRequest.setName("佐々木伸二");
+			userSearchRequest.setAge(29);
+			userSearchRequest.setBirthday(LocalDate.of(1991,10,29));
+			// createはvoid型メソッドなので、戻り値はdoNothing()を用いる。
+			doNothing().when(userService).create(userSearchRequest);
+			
+			mockMvc.perform(
+				post("/user/create")
+				.flashAttr("userSearchRequest", userSearchRequest));
+			
+			verify(userService, times(1)).create(userSearchRequest);
+		}
+			
 		@Test
 		@DisplayName(" 正常時、/にリダイレクトする")
 		void  正常時にリダイレクトする() throws Exception {
@@ -179,6 +213,7 @@ class UserControllerTest {
 			userSearchRequest.setName("佐々木伸二");
 			userSearchRequest.setAge(29);
 			userSearchRequest.setBirthday(LocalDate.of(1991,10,29));
+			doNothing().when(userService).create(userSearchRequest);
 			
 			mockMvc.perform(
 				post("/user/create")
@@ -186,6 +221,126 @@ class UserControllerTest {
 			).andExpect(status().isFound())
 			.andExpect(redirectedUrl("/"));
 		}
+	}
+	
+	@Nested
+	class ユーザー情報更新画面を取得_また既存データを流す {
 		
+		@Test
+		@DisplayName("user/update.htmlを返す")
+		void user_search_htmlを返す() throws Exception {
+			User user = new User();
+			int id = 1;
+			doReturn(user).when(userService).search(id);
+			
+			mockMvc.perform(
+					get("/user/update/id=1"))
+			.andExpect(status().isOk())
+			.andExpect(view().name("user/update"));
+		}
+		
+		@Test
+		@DisplayName(" ユーザー情報取得のサービスメソッドが呼ばれている")
+		void  ユーザー情報取得のサービスメソッドが呼ばれている() throws Exception {
+			
+			User user = new User();
+			int id = 1;
+			doReturn(user).when(userService).search(id);
+			
+			mockMvc.perform(
+				get("/user/update/id=1"))
+				.andExpect(status().isOk());
+			
+			verify(userService, times(1)).search(1);
+		}
+		
+		@Test
+		@DisplayName("modelに変数userinfoでUser型のデータを流す")
+		void modelに変数userinfoでUser型のデータを流す() throws Exception {
+			
+			// 前処理
+			User user = new User();
+			int id = 1;
+			doReturn(user).when(userService).search(id);
+			
+			// 実行&検証
+			mockMvc.perform(
+				get("/user/update/id=1")
+			).andExpect(status().isOk())
+			.andExpect(model().attribute("userinfo", userService.search(id)))
+			.andExpect(model().hasNoErrors());
+		}
+	}
+	
+	@Nested
+	class ユーザー更新を行い_ホームにリダイレクトする {
+		
+		@Test
+		@DisplayName("ユーザー情報更新のサービスメソッドが実行できているか")
+		void  正常時にユーザー登録のサービスメソッドが実行できているか() throws Exception {
+			// 前処理
+			UserSearchRequest user = new UserSearchRequest();
+			user.setId(25);
+			user.setName("佐々木伸二");
+			user.setAge(29);
+			user.setBirthday(LocalDate.of(1991,10,29));
+			doNothing().when(userService).update(user.getId(), user.getName(), user.getAge(), user.getBirthday());
+			
+			mockMvc.perform(
+				post("/user/update/id=25")
+				.flashAttr("userSearchRequest", user))
+				.andExpect(status().isFound());
+			
+			verify(userService, times(1)).update(user.getId(), user.getName(), user.getAge(), user.getBirthday());
+		}
+			
+		@Test
+		@DisplayName(" 正常時、/にリダイレクトする")
+		void  正常時にリダイレクトする() throws Exception {
+			// 前処理
+			UserSearchRequest user = new UserSearchRequest();
+			user.setId(25);
+			user.setName("佐々木伸二");
+			user.setAge(29);
+			user.setBirthday(LocalDate.of(1991,10,29));
+			doNothing().when(userService).update(user.getId(), user.getName(), user.getAge(), user.getBirthday());
+			
+			mockMvc.perform(
+				post("/user/update/id=25")
+				.flashAttr("userSearchRequest", user)
+			).andExpect(status().isFound())
+			.andExpect(redirectedUrl("/"));
+		}
+	}
+	
+	@Nested
+	class ユーザー情報の削除を行いホームにリダイレクトする {
+		
+		@Test
+		@DisplayName("ユーザー情報削除のサービスメソッドが実行できているか")
+		void  ユーザー情報削除のサービスメソッドが実行できているか() throws Exception {
+			// 前処理
+			int id = 25;
+			doNothing().when(userService).delete(id);
+			
+			mockMvc.perform(
+				post("/user/delete/id=25"))
+				.andExpect(status().isFound());
+			
+			verify(userService, times(1)).delete(id);
+		}
+			
+		@Test
+		@DisplayName(" 削除後、/にリダイレクトする")
+		void  削除後リダイレクトする() throws Exception {
+			// 前処理
+			int id = 25;
+			doNothing().when(userService).delete(id);
+			
+			mockMvc.perform(
+				post("/user/delete/id=25")
+			).andExpect(status().isFound())
+			.andExpect(redirectedUrl("/"));
+		}
 	}
 }
